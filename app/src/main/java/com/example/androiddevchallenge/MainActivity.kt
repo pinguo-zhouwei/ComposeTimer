f8d72f36d245b0ec1cc5baacd49977eee6e81e35
zhouwei <zhouweigmail@163.com>
2021-03-07 11:20:38 +0800

    fix: pass lint

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,44 +19,44 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Text
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Button
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.androiddevchallenge.viewmodle.ClockViewModel
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.color_50
 import com.example.androiddevchallenge.ui.theme.color_cc
+import com.example.androiddevchallenge.viewmodle.ClockViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -137,10 +137,8 @@ fun Timer() {
                 }
             }
         }
-
     }
 }
-
 
 @Composable
 fun Clock(viewModel: ClockViewModel) {
--- app/src/main/java/com/example/androiddevchallenge/viewmodle/ClockViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/viewmodle/ClockViewModel.kt
@@ -1,11 +1,24 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.viewmodle
-
 
 import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
 
 class ClockViewModel : ViewModel() {
     private val _time = MutableLiveData(0L)
@@ -20,12 +33,11 @@ class ClockViewModel : ViewModel() {
     fun start(time: Long) {
         countDownTimer = object : CountDownTimer(time * 1000, 1000) {
             override fun onFinish() {
-
             }
 
             override fun onTick(millisUntilFinished: Long) {
                 _time.value = millisUntilFinished / 1000
-                _progress.value =  (1- (millisUntilFinished / 1000 * 1.0f / time ))
+                _progress.value = (1 - (millisUntilFinished / 1000 * 1.0f / time))
             }
         }
         countDownTimer.start()
