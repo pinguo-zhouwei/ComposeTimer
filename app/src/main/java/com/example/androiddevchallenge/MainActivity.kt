0b01c544efa459a42aed9b175c96ca572742bc2c
zhouwei <zhouweigmail@163.com>
2021-03-07 10:31:17 +0800

    fix: compose timer

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,14 +18,29 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.viewmodle.ClockViewModel
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.color_50
+import com.example.androiddevchallenge.ui.theme.color_cc
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,8 +55,101 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column {
+            TopAppBar(
+                elevation = 4.dp,
+                contentPadding = PaddingValues(top = 20.dp, start = 10.dp),
+            ) {
+                Text(
+                    text = "CountDownTimer",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Timer()
+        }
     }
+}
+
+@Composable
+fun Timer() {
+    val clockViewModel: ClockViewModel = viewModel()
+    var textInput by remember { mutableStateOf("") }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(Color(0xFFDBDDEB))
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+            ) {
+                OutProgress(clockViewModel)
+                Clock(clockViewModel)
+            }
+            Spacer(modifier = Modifier.height(60.dp))
+            TextField(
+                value = textInput,
+                onValueChange = { textInput = it },
+                label = { Text("Input Time") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.width(280.dp)
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.width(280.dp),
+            ) {
+                Button(
+                    onClick = { clockViewModel.start(textInput.toLong()) },
+                    modifier = Modifier.size(100.dp, 50.dp),
+                ) {
+                    Text("Start")
+                }
+                Button(
+                    onClick = { clockViewModel.cancel() },
+                    modifier = Modifier.size(100.dp, 50.dp),
+                ) {
+                    Text("Cancel")
+                }
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun Clock(viewModel: ClockViewModel) {
+    val time by viewModel.time.observeAsState(initial = 0)
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .background(color = color_cc, shape = CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$time",
+            color = Color.Black,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun OutProgress(clockViewModel: ClockViewModel) {
+    val progress by clockViewModel.progress.observeAsState(initial = 0f)
+    CircularProgressIndicator(
+        progress = progress,
+        strokeWidth = 10.dp,
+        modifier = Modifier.size(250.dp),
+        color = color_50
+    )
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
---
+++ app/src/main/java/com/example/androiddevchallenge/viewmodle/ClockViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.androiddevchallenge.viewmodle
+
+
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.properties.Delegates
+
+
+class ClockViewModel : ViewModel() {
+    private val _time = MutableLiveData(0L)
+    private val _progress = MutableLiveData(0.1f)
+    private lateinit var countDownTimer: CountDownTimer
+    val time: LiveData<Long> = _time
+    val progress: LiveData<Float> = _progress
+
+    /**
+     * start countdown
+     */
+    fun start(time: Long) {
+        countDownTimer = object : CountDownTimer(time * 1000, 1000) {
+            override fun onFinish() {
+
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                _time.value = millisUntilFinished / 1000
+                _progress.value =  (1- (millisUntilFinished / 1000 * 1.0f / time ))
+            }
+        }
+        countDownTimer.start()
+    }
+
+    fun cancel() {
+        countDownTimer?.cancel()
+    }
+}
